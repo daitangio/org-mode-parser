949d39005e76ebf005afadeb31902de58cb47065
Giovanni Giorgi <jj@gioorgi.com>
2011-10-10 13:58:32 +0200

    Release script

--- bin/releaseVersion.sh
+++ bin/releaseVersion.sh
@@ -1,25 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 # Internal housekeeping only
 # Example $0 ORG_MODE_PARSER_0.0.1
 set -e -x
-#Reset every thing not yet commited (security measure)
-(cd $HOME/org-mode-parser ; git reset --hard HEAD)
-tagVersion=$1
-cd $(dirname $0)/../..
-pwd
-test  -d ../tempHgFromRepo && rm -rf ../tempHgFromRepo
-hg clone --quiet -u $tagVersion . ../tempHgFromRepo 
-cd ../tempHgFromRepo
-# Take the stuff and copy on the git destination
-cp -pr org-mode-parser/* $HOME/org-mode-parser
-
-## hg log --style changelog  -r $tagVersion >>$HOME/org-mode-parser/CHANGELOG
-cd $HOME/org-mode-parser
-
-#git reset --hard HEAD
+$(dirname $0)/testInstall.sh || exit 1000
+echo Check the status
+git commit  --dry-run -a -m "Riepilogo" 
+read -e  -p 'Proceed (y/n)?' -i y reply
+# exit if not yes
+test "$reply" != "y"  && exit
+echo Commit...
 git status
 git add -i -v
-# git commit -a -v  -m "Delivered tag revision $tagVersion"
-# git tag -a -m "Revision $tagVersion"  $tagVersion
-# git tag
-# git push -v --tags
+git commit -a -v  -m "Delivered tag revision $tagVersion"
+git tag -a -m "Revision $tagVersion"  $tagVersion
+git tag
+git push -v --tags
+git push -v
