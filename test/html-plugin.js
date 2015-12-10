65eb11631c036b85bf48b62cf0bb8368c6bbba0e
Giovanni Giorgi <giovanni.giorgi@nttdata.com>
2015-12-10 18:22:59 +0100

    First html render is here

--- test/html-plugin.js
+++ test/html-plugin.js
@@ -14,17 +14,11 @@ var orgParser=require("../lib/org-mode-parser");
 // Create a Test Suite
 vows.describe('OrgMode Html plugin').addBatch({
     'basicLibraries':{
-        'quickcheck1':{
+        'html':{
             // Very stupid, only for getting quickcheck on track
-            'qc1':function() {
-
-                function randomParser(body) { 
-		    var n=orgParser.parseBigString("* Very Stupid\nData Line1\n"+body);		
-		    return n[0].body.match("Data Line1\n.*");
-	        }
-                
-                // Check a bunch of random strings...
-                assert.equal(true,qc.forAll(randomParser,qc.arbString));
+            'vanilla':function() {
+                var n=new orgParser.OrgQuery(orgParser.parseBigString("* Very Stupid\nData Line1\n"));		
+                assert.equal("<h1>Very Stupid</h1><p>Data Line1</p><p></p>",n.toHtml());
             }
         }
     }
